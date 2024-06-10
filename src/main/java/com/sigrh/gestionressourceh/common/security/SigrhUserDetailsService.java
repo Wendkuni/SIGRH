be66bb133b4d32d36d8104cda244639ae72336ab
arnaudk03 <78480081+arnaudk03@users.noreply.github.com>
2024-06-10 21:34:14 +0000

    implementation security

--- src/main/java/com/sigrh/gestionressourceh/common/security/SigrhUserDetailsService.java
+++ src/main/java/com/sigrh/gestionressourceh/common/security/SigrhUserDetailsService.java
@@ -1,49 +1,49 @@
-package com.sigrh.gestionressourceh.common.security;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-import com.did.ersto.user.model.UserModel;
-import com.did.ersto.user.model.UserRoleModel;
-import com.did.ersto.user.service.UserRoleService;
-import com.did.ersto.user.service.UserService;
-
-
-@Component
-public class SigrhUserDetailsService implements UserDetailsService {
-
-	//@Cacheable(value = "user", key = "#username")
-	@Override
-	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-		UserModel user = UserService.selectByUsername(username);
-		if(user == null) throw new UsernameNotFoundException(username);
-		List<UserRoleModel> roles = UserRoleService.selectByUser(user);
-		Collection<GrantedAuthority> authorities = new ArrayList<>();
-		for (UserRoleModel userRole : roles) {
-			authorities.add(new SimpleGrantedAuthority(userRole.getRole().getCode()));
-		}
-		//return new User(user.getUsername(), user.getPassword(), authorities);
-		return new SigrhUser(user.getUsername(), user.getPassword(), authorities, user.getPersonnel());
-	}
-	
-	public String getPrincipal() {
-		String username = "anonymous";
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (!(authentication instanceof AnonymousAuthenticationToken)) {
-		   username = authentication.getName();
-		}
-		return username;
-	}
-
-}
+//package com.sigrh.gestionressourceh.common.security;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//
+//import org.springframework.security.authentication.AnonymousAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//import com.did.ersto.user.model.UserModel;
+//import com.did.ersto.user.model.UserRoleModel;
+//import com.did.ersto.user.service.UserRoleService;
+//import com.did.ersto.user.service.UserService;
+//
+//
+//@Component
+//public class SigrhUserDetailsService implements UserDetailsService {
+//
+//	//@Cacheable(value = "user", key = "#username")
+//	@Override
+//	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+//		UserModel user = UserService.selectByUsername(username);
+//		if(user == null) throw new UsernameNotFoundException(username);
+//		List<UserRoleModel> roles = UserRoleService.selectByUser(user);
+//		Collection<GrantedAuthority> authorities = new ArrayList<>();
+//		for (UserRoleModel userRole : roles) {
+//			authorities.add(new SimpleGrantedAuthority(userRole.getRole().getCode()));
+//		}
+//		//return new User(user.getUsername(), user.getPassword(), authorities);
+//		return new SigrhUser(user.getUsername(), user.getPassword(), authorities, user.getPersonnel());
+//	}
+//
+//	public String getPrincipal() {
+//		String username = "anonymous";
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+//		   username = authentication.getName();
+//		}
+//		return username;
+//	}
+//
+//}
--- src/main/java/com/sigrh/gestionressourceh/dtos/user/LoginDtoRequest.java
+++ src/main/java/com/sigrh/gestionressourceh/dtos/user/LoginDtoRequest.java
@@ -1,30 +1,30 @@
 package com.sigrh.gestionressourceh.dtos.user;
 
-import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.NotNull;
 
 
 
 public class LoginDtoRequest {
-	@NotNull(message = "username est requis")
-	private String username;
-	@NotNull(message = "password est requis")
-	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+//	@NotNull(message = "username est requis")
+//	private String username;
+//	@NotNull(message = "password est requis")
+//	private String password;
+//
+//	public String getUsername() {
+//		return username;
+//	}
+//
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
 	
     
 
