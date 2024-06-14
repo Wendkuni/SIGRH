bd02cfefeb59fcb8600214905768277e72b53512
antoinekonate@gmail.com <antoinekonate@gmail.com>
2024-06-14 13:51:33 +0100

    type de valIndemite

--- src/main/java/com/sigrh/gestionressourceh/domains/comptablite/ConfigIdemit.java
+++ src/main/java/com/sigrh/gestionressourceh/domains/comptablite/ConfigIdemit.java
@@ -5,13 +5,14 @@ import com.sigrh.gestionressourceh.domains.parametres.FonctionListeModel;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @Getter@Setter@AllArgsConstructor@NoArgsConstructor@Builder
 public class ConfigIdemit {
     private long idConfigIdemit;
     private FonctionListe idFonctionListe;
     private String libIdnmite;
-    private Boolean valIdeminite;
-    private LocalDate dateCreat;
+    private int valIdeminite;
+    private Date dateCreat;
 }
 
