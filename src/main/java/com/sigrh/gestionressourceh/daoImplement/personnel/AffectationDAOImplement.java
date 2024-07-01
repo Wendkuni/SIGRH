a0b2e61d533f55ee8a8f3b5b7dbaab4f3578c912
antoinekonate@gmail.com <antoinekonate@gmail.com>
2024-07-01 19:07:40 +0100

    Affectation ok

--- src/main/java/com/sigrh/gestionressourceh/daoImplement/personnel/AffectationDAOImplement.java
+++ src/main/java/com/sigrh/gestionressourceh/daoImplement/personnel/AffectationDAOImplement.java
@@ -34,12 +34,34 @@ public class AffectationDAOImplement implements DAOTemplete<PersonnelAffectation
     @Override
     public boolean create(PersonnelAffectationModel obj) {
         try {
-            String sql = "Insert into affectation(IDaffectation,dateffet,DREN,localite,NOTEPEDAGOCIQ,IDagent,serviceecole,motif,situationsanit,imgsanit,libeldoss,imgdos,nombrefant,ancienetegen,ancieneteposte,posteorigin,postedestinat1,postedestinat2,postedestinat3) " +
+            Map<String, Object> params = new HashMap<>();
+            params.put("dateffet", obj.getDateEffet());
+            params.put("DREN",obj.getDren() );
+            params.put("localite",obj.getLocalite() );
+            params.put("NOTEPEDAGOCIQ",obj.getNotePedagogiq() );
+            params.put("IDagent",obj.getPersonnel().getIdAgent() );
+            params.put("serviceecole",obj.getServiceEcole() );
+            params.put("motif",obj.getMotif() );
+            params.put("situationsanit",obj.getSituationSanit() );
+            params.put("imgsanit",obj.getImgSanit() );
+            params.put("libeldoss",obj.getLibelDos() );
+            params.put("imgdos",obj.getImgDos() );
+            params.put("nombrefant",obj.getNombreFant( ));
+            params.put("ancienetegen",  obj.getAncienetePoste());
+            params.put("posteorigin", obj.getPosteOrigin());
+            params.put("postedestinat1",obj.getPosteDestinat1() );
+            params.put("postedestinat2",obj.getPosteDestinat2() );
+            params.put("postedestinat3", obj.getPosteDestinat3());
+            Number newId = insert.executeAndReturnKey(params);
+            obj.setIdAffectation(newId.longValue());
+
+            return newId.intValue()!=0;
+            /*String sql = "Insert into affectation(IDaffectation,dateffet,DREN,localite,NOTEPEDAGOCIQ,IDagent,serviceecole,motif,situationsanit,imgsanit,libeldoss,imgdos,nombrefant,ancienetegen,ancieneteposte,posteorigin,postedestinat1,postedestinat2,postedestinat3) " +
                     "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             return  jdbcTemplate.update(sql,obj.getIdAffectation(), obj.getDateEffet(),  obj.getDren(), obj.getLocalite(),
                     obj.getNotePedagogiq(),obj.getPersonnel().getIdAgent(),obj.getServiceEcole(),
                     obj.getMotif(), obj.getSituationSanit(), obj.getImgSanit(), obj.getLibelDos(), obj.getImgDos(), obj.getNombreFant(),
-                    obj.getAncieneteGen(), obj.getAncienetePoste(), obj.getPosteOrigin(),obj.getPosteDestinat1(),obj.getPosteDestinat2(),obj.getPosteDestinat3() )!=0;
+                    obj.getAncieneteGen(), obj.getAncienetePoste(), obj.getPosteOrigin(),obj.getPosteDestinat1(),obj.getPosteDestinat2(),obj.getPosteDestinat3() )!=0;*/
         }catch (Exception e) {
             e.printStackTrace();
             return false;
