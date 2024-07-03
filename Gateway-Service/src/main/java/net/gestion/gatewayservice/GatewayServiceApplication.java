cea786352f3c5877b56577205f6f53d1201be3a9
antoinekonate@gmail.com <antoinekonate@gmail.com>
2024-07-03 05:42:35 +0100

    projet revu-en-microservice

--- Gateway-Service/src/main/java/net/gestion/gatewayservice/GatewayServiceApplication.java
+++ Gateway-Service/src/main/java/net/gestion/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,20 @@ package net.gestion.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
+
+
 
 }
